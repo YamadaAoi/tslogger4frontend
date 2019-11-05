5bd7477d050459845147fe19fe095412cd1912aa
周胤奎 <zhouyinkui@mlogcn.com>
2019-11-05 15:18:01 +0800

    删除编译文件

--- dist/types/tsLog.util.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type action = "INFO" | "ERROR" | "WARN";
-export interface LogItem {
-    log: string;
-    date: number;
-    type: action;
-}
-export declare function download(filename: string, text: string): void;
-export declare function dateFormat(date: number, fmt?: string): string;
-//# sourceMappingURL=tsLog.util.d.ts.map
--- dist/types/tsLogService.d.ts
+++
@@ -1,9 +0,0 @@
-export default interface TsLogService {
-    usetsLog?: boolean;
-    doConsole?: boolean;
-    itemName?: string;
-    maxLen?: number;
-    logName?: string;
-    [name: string]: any;
-}
-//# sourceMappingURL=tsLogService.d.ts.map
